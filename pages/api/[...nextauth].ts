47eafb60054c362f1c679f0f4551ebbd8c711a90
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-10-14 11:24:24 +0700

    add user registration endpoint with bcrypt password hashing

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -12,44 +12,44 @@ export default NextAuth({
       credentials: {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials) {
-            if (credentials?.email || credentials?.password) {
-                throw new Error("Email and password required");
-            }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password required");
+        }
 
-            const user = await prismadb.user.findUnique({
-                where: {
-                    email: credentials.email,
-                },
-            });
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-            if (!user || user.hashedPassword) {
-                throw new Error("Email does not exist");
-            }
+        if (!user || user.hashedPassword) {
+          throw new Error("Email does not exist");
+        }
 
-            const isPasswordValid = await compare(credentials.password, user.hashedPassword);
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
 
-            if (!isPasswordValid) {
-                throw new Error("Invalid password");
-            }
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
+        }
 
-            return user;
-        }),
-      
-    ],
-    pages: {
-        signIn: "/auth",
-
-    },
-    debug: process.env.NODE_ENV === "development",
-    session: {
-        strategy: "jwt",
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET,
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    
-
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
