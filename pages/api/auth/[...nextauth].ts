8c97f676773ae901f32f973173068afbc12db08f
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-10-15 11:01:12 +0700

    fix bugs google OAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -17,8 +17,8 @@ export default NextAuth({
     }),
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID || "",
-      clientSecret: process.env.GOOGLE_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
 
     Credentials({
