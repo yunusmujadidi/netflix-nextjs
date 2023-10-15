9ec67b9db267273e910a08c6a1fcf8253c58ce9d
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-10-15 10:49:34 +0700

    add github and google OAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,26 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import prismadb from "../../../lib/prismadb";
+
 import { compare } from "bcrypt";
+
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+
     Credentials({
       id: "credentials",
       name: "Credentials",
@@ -45,6 +60,7 @@ export default NextAuth({
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismadb),
   session: {
     strategy: "jwt",
   },
