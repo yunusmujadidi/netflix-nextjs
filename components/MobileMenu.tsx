fcd0c107c0fb16667bf3cbb69382b1c51bd3eb47
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-10-16 20:16:58 +0700

    finishing navbar

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -6,7 +6,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex ">
+    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 ">
       <div className="flex flex-col gap-4">
         <div className="px-3 text-center text-white hover:underline">Home</div>
         <div className="px-3 text-center text-white hover:underline">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,54 @@
 import Image from "next/image";
-import { BsChevronDown } from "react-icons/bs";
-import { useState } from "react";
+import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
+
+import { useEffect, useState } from "react";
 import { useCallback } from "react";
 
 import NavbarItem from "../components/NavbarItem";
 import MobileMenu from "./MobileMenu";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((prev) => !prev);
+  }, []);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((prev) => !prev);
   }, []);
+
   return (
     <nav className="fixed w-full z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+      <div
+        className={`
+          px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ${
+            showBackground ? "bg-zinc-900 bg-opacity-90" : "bg-transparent"
+          }
+        `}
+      >
         <Image src="/images/logo.png" width={100} height={140} alt="logo" />
-        <div className="flex-row gap-7 ml-7 hidden lg:flex">
+        <div className="flex-row gap-7 ml-8 hidden lg:flex">
           <NavbarItem label="Home" />
           <NavbarItem label="Series" />
           <NavbarItem label="Film" />
@@ -26,11 +58,43 @@ const Navbar = () => {
         </div>
         <div
           onClick={toggleMobileMenu}
-          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer"
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
         >
           <p className="text-white text-sm">Browse</p>
-          <BsChevronDown className="text-white transition" />
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
           <MobileMenu isOpen={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer ">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer ">
+            <BsBell />
+          </div>
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center cursor-pointer gap-2 relative"
+          >
+            <div>
+              <Image
+                className="w-8 rounded-md"
+                src="/images/default-blue.png"
+                width={300}
+                height={300}
+                alt="avatar"
+              />
+            </div>
+            <BsChevronDown
+              className={`text-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu isOpen={showAccountMenu} />
+          </div>
         </div>
       </div>
     </nav>
