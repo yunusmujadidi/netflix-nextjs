53aac5f7ab4e1a70dc7102e5e2212404d2951256
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-10-16 18:37:32 +0700

    dropdown menu on navbar

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,27 @@
+interface MobileMenuProps {
+  isOpen?: boolean;
+}
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex ">
+      <div className="flex flex-col gap-4">
+        <div className="px-3 text-center text-white hover:underline">Home</div>
+        <div className="px-3 text-center text-white hover:underline">
+          Series
+        </div>
+        <div className="px-3 text-center text-white hover:underline">Films</div>
+        <div className="px-3 text-center text-white hover:underline">
+          New & Popular
+        </div>
+        <div className="px-3 text-center text-white hover:underline">
+          Browse by languages
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
+import { BsChevronDown } from "react-icons/bs";
+import { useState } from "react";
+import { useCallback } from "react";
+
 import NavbarItem from "../components/NavbarItem";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((prev) => !prev);
+  }, []);
   return (
     <nav className="fixed w-full z-40">
       <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
@@ -14,7 +24,14 @@ const Navbar = () => {
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by languages" />
         </div>
-        <div></div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer"
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown className="text-white transition" />
+          <MobileMenu isOpen={showMobileMenu} />
+        </div>
       </div>
     </nav>
   );
